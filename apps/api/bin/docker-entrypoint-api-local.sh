cfd25c1f95df5709917ba1533dd171237cfab8ba
Ivan camilo gudiño ochoa <ivan.gudino@campusucc.edu.co>
2026-07-07 18:52:29 -0500

    rev2

--- apps/api/bin/docker-entrypoint-api-local.sh
+++ apps/api/bin/docker-entrypoint-api-local.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+# Load environment variables from .env file if present
+[ -f .env ] && set -a && source .env && set +a
 python manage.py wait_for_db
 # Wait for migrations
 python manage.py wait_for_migrations
